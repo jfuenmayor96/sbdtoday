6086a904d1f68f64e8c20e4f48e953d26a32ef1f
Julio Fuenmayor <jfuenmayor96@gmail.com>
2017-08-06 20:28:52 -0400

    se agrega un sitemap del sitio

--- client/src/App.js
+++ client/src/App.js
@@ -79,7 +79,7 @@ class App extends Component {
           <span className="glyphicon glyphicon-chevron-down"></span>
         </Link>
         <div className="row principal">
-          <div className="col-lg-4 col-md-4 col-sm-4 col-xs-6 monedas">
+          <div className="col-lg-4 col-md-5 col-sm-6 col-xs-10 monedas">
             <label htmlFor="sbd">SBD </label><input type="text" id="sbd" onKeyUp={this.actualizarDesdeSBD}/><br/>
             <label htmlFor="bitcoin">Bitcoin </label><input type="text" id="bitcoin" onKeyUp={this.actualizarDesdeBitcoin}/><br/>
             <label htmlFor="bsf">Bolívares </label><input type="text" id="bsf" onKeyUp={this.actualizarDesdeBolivares}/>
@@ -91,17 +91,21 @@ class App extends Component {
           </div>
           <div className="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1" style={{color: "black"}}>
             <h2>¿Qué es SBDToday?</h2>
-            <p>SBDToday surge como una idea pensada para los usuarios venezolanos que hacen vida en la comunidad de Steemit con el fin de que éstos puedan saber cuántos Bolívares y Bitcoins representan sus ganancias en SBD.<br/><br/>
-            <p>Este proyecto cuenta con el apoyo y patrocinio del grupo <a href="https://steemit.com/@provenezuela">@provenezuela</a> en Steemit. </p>
+            <p>SBDToday surge como una idea pensada para los usuarios venezolanos que hacen vida en la comunidad de Steemit con el fin de que éstos puedan tener un valor de referencia sobre cuántos Bolívares y Bitcoins representan sus ganancias en SBD.<br/><br/>
+            <p>Este proyecto cuenta con el apoyo y patrocinio del grupo <a href="https://steemit.com/@provenezuela">@provenezuela</a> en Steemit.</p> <br/>
+            <div style={{display: "flex", justifyContent: "center"}}>
+              <a href="https://steemit.com/@provenezuela" alt="Perfil de steemit de provenezuela"><img className="img-responsive" src={require("./assets/provenezuela.png")} alt="Logo de provenezuela"/></a>
+            </div>
+
             </p>
           </div>
           <div className="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1" style={{color: "black", paddingTop: "15px", paddingBottom: "15px"}}>
-            <h2>¿Cómo hacen la conversión?</h2>
-            <p>Cada vez que se ingresa a SBDToday se descarga la última información disponible de las tasas de cambio. Las tasas de cambio utilizadas son las proporcionadas por las APIs de <b>DolarToday</b> para la conversión de <b>Dólares a Bolívares</b>, la de <b>Bittrex</b> para convertir de <b>SBD a Bitcoin</b> y la de <b>Bitfinex</b> para convertir de <b>Bitcoin a Dólares</b>. En ningún momento SBDToday establece algún tipo de tasa de cambio.</p>
+            <h2>¿Cómo funciona?</h2>
+            <p>Es tan simple como colocar el valor que deseas convertir en el recuadro correspondiente a su moneda, y automáticamente los otros dos campos se actualizarán con su respectiva conversión.</p>
           </div>
-          <div className="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1" style={{color: "black", paddingBottom: "15px"}}>
-            <h2>¿Tienen algún tipo de relación con DolarToday?</h2>
-            <p>No, SBDToday no tiene ningún tipo de relación con DolarToday.</p>
+          <div className="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1" style={{color: "black", paddingTop: "15px", paddingBottom: "15px"}}>
+            <h2>¿Cómo hacen la conversión?</h2>
+            <p>Cada vez que se ingresa a SBDToday se descarga la última información disponible de las tasas de cambio proporcionadas por las APIs de <b>Bittrex</b> para convertir de <b>SBD a Bitcoin</b> y la de <b>Bitfinex</b> para convertir de <b>Bitcoin a Dólares</b>. En ningún momento SBDToday establece algún tipo de tasa de cambio. Todos los valores aquí presentes deben ser considerados como valores de referencia.</p>
           </div>
         </div>
         <br/>
@@ -112,18 +116,16 @@ class App extends Component {
             </div>
             <div className="col-lg-6 col-md-6 visible-lg visible-md hidden-xs hidden-sm" style={{backgroundColor: "#1A5099"}}>
                 <p style={{textAlign: "right"}}>
-                    <a href="https://jfuenmayor96.github.io"><i className="fa fa-2x fa-briefcase" aria-hidden="true"></i></a>
-                    <a href="https://linkedin.com/in/jefuenmayor"><i className="fa fa-2x fa-linkedin-square" aria-hidden="true"></i></a>
-                    <a href="https://github.com/jfuenmayor96"><i className="fa fa-2x fa-github" aria-hidden="true"></i></a>
-                    <a href="https://medium.com/@jfuenmayor96"><i className="fa fa-2x fa-medium" aria-hidden="true"></i></a>
+                    <a href="https://jfuenmayor96.github.io" alt="Portafolio de Julio Fuenmayor"><i className="fa fa-2x fa-briefcase" aria-hidden="true"></i></a>
+                    <a href="https://linkedin.com/in/jefuenmayor" alt="Linkedin de Julio Fuenmayor"><i className="fa fa-2x fa-linkedin-square" aria-hidden="true"></i></a>
+                    <a href="https://github.com/jfuenmayor96" alt="Github de Julio Fuenmayor"><i className="fa fa-2x fa-github" aria-hidden="true"></i></a>
                 </p>
             </div>
             <div className="col-sm-12 col-xs-12 visible-sm visible-xs hidden-lg hidden-md" style={{backgroundColor: "#1A5099"}}>
                 <p style={{textAlign: "center"}}>
-                    <a href="https://jfuenmayor96.github.io"><i className="fa fa-2x fa-briefcase" aria-hidden="true"></i></a>
-                    <a href="https://linkedin.com/in/jefuenmayor"><i className="fa fa-2x fa-linkedin-square" aria-hidden="true"></i></a>
-                    <a href="https://github.com/jfuenmayor96"><i className="fa fa-2x fa-github" aria-hidden="true"></i></a>
-                    <a href="https://medium.com/jfuenmayor96"><i className="fa fa-2x fa-medium" aria-hidden="true"></i></a>
+                    <a href="https://jfuenmayor96.github.io" alt="Portafolio de Julio Fuenmayor"><i className="fa fa-2x fa-briefcase" aria-hidden="true"></i></a>
+                    <a href="https://linkedin.com/in/jefuenmayor" alt="Linkedin de Julio Fuenmayor"><i className="fa fa-2x fa-linkedin-square" aria-hidden="true"></i></a>
+                    <a href="https://github.com/jfuenmayor96" alt="Github de Julio Fuenmayor"><i className="fa fa-2x fa-github" aria-hidden="true"></i></a>
                 </p>
             </div>
             <div className='col-sm-12 col-xs-12 visible-sm visible-xs hidden-lg hidden-md' style={{textAlign: "center", backgroundColor: "#1A5099", height: "auto", paddingTop: "10px"}}>
