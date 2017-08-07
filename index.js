da7dc902dffb368cb59e4e0e5d80be029d83468b
Julio Fuenmayor <jfuenmayor96@gmail.com>
2017-08-07 00:31:48 -0400

    se arregla la estructura del directorio

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 var axios = require('axios');
 var path = require('path');
 
-
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -41,6 +41,13 @@ app.get('/api/USD-VEF', (req, res) => {
         console.log(error.response.headers);}})
 });
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  console.log(path.join(__dirname+'/client/build/index.html'));
+});
+
 
 const port = process.env.PORT || 5000;
 app.listen(port);
+
+console.log("SBDToday running on port " + port);
